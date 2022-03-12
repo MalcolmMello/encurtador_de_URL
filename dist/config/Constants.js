f5ac14b5222b1d6c17a209b54352ca18fa96d19f
MalcolmMello <malcolmmellolima@gmail.com>
2022-03-11 21:02:05 -0300

    finalizado

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 'mongodb+srv://MalcolmMello:27012005@url-shortner.bkyeg.mongodb.net/url-shortner?retryWrites=true&w=majority'
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION:
+        'mongodb+srv://MalcolmMello:27012005@url-shortner.bkyeg.mongodb.net/url-shortner?retryWrites=true&w=majority'
+}
